2430c8d12e40ce5578fe9e7aaf32cb1eee8e2639
ncore8868 <n.core8868@gmail.com>
2026-08-20 16:22:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+/* UNION ONE 진행판 - 런처 서비스워커
+   앱 화면은 항상 서버에서 새로 받아야 하므로 캐시하지 않는다.
+   여기서는 런처 껍데기와 아이콘만 캐시한다. */
+
+const CACHE = 'unionone-launcher-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+
+  // 구글 앱스스크립트 요청은 절대 가로채지 않는다
+  if (url.hostname.indexOf('google') >= 0) return;
+  if (e.request.method !== 'GET') return;
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
